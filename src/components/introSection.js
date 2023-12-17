5f5acf08bd9c1c92803277575b28310377a9514b
Lukas Hedström <lukashedstrom@Lukass-MacBook-Air.local>
2023-12-17 15:57:12 +0100

    Fixes

--- src/components/introSection.js
+++ src/components/introSection.js
@@ -1,108 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilRuler, faCode, faDatabase, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-
-const outerContainerStyle = {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    height: '80vh',
-    background: 'linear-gradient(90deg, rgba(131,58,180,1), rgba(253,29,29,1), rgba(252,176,69,1))',
-};
-
-const introSectionStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '20px 40px',
-    flex: 1,
-};
-
-const nameTextStyle = {
-    fontSize: '2.5rem',
-    fontFamily: 'Raleway',
-    fontWeight: '900',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    marginLeft: '150px',
-    zIndex: 3,
-    //background: 'linear-gradient(90deg, rgba(253,29,29,1), rgba(252,176,69,1), rgba(131,58,180,1))',
-    background: 'white',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-    MozBackgroundClip: 'text',
-    MozTextFillColor: 'transparent',
-    backgroundClip: 'text',
-    color: 'transparent',
-};
-
-const descripTextStyle = {
-    fontSize: '1.5rem',
-    fontFamily: 'Raleway',
-    color: 'white',
-    textAlign: 'left',
-    marginLeft: '150px',
-    maxWidth: '600px',
-};
-
-const textContainer = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    zIndex: 3,
-};
-
-const introImageStyle = {
-    width: '400px',
-    height: '400px',
-    borderRadius: '200px',
-    backgroundImage: 'url(/clear4.png)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center 70%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 3,
-    marginRight: '200px',
-    marginBottom: '0px',
-};
-
-const footerOuterStyle = {
-    background: 'linear-gradient(90deg, rgba(131,58,180,1), rgba(253,29,29,1), rgba(252,176,69,1))',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '30px',
-};
-
-const footerInnerStyle = {
-    background: 'linear-gradient(135deg, rgba(131,58,180,1), rgba(253,29,29,1), rgba(252,176,69,1))',
-    height: '100%',
-    width: '85%',
-    borderRadius: '85px',
-    boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.4)',
-    position: 'relative',
-};
-
-// Style for the down arrow icon
-const downArrowStyle = {
-    color: 'white',
-    fontSize: '2em',
-    position: 'absolute',
-    top: 'calc(80vh + 30px)',
-    left: '50%',
-    transform: 'translateX(-50%)',
-};
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const IntroSection = () => {
+    const [animateFooter, setAnimateFooter] = useState(false);
+    const [showDownArrow, setShowDownArrow] = useState(false);
+    const [downArrowOpacity, setDownArrowOpacity] = useState(0);
+
+    useEffect(() => {
+        const footerTimer = setTimeout(() => setAnimateFooter(true), 300);
+        const arrowTimer = setTimeout(() => {
+            setShowDownArrow(true);
+            setDownArrowOpacity(0.8); // Set opacity to 1 for fade-in effect
+        }, 5000); // 5 seconds delay
+
+        return () => {
+            clearTimeout(footerTimer);
+            clearTimeout(arrowTimer);
+        };
+    }, []);
+
+    const outerContainerStyle = {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '80vh',
+        background: 'linear-gradient(90deg, rgba(131,58,180,1), rgba(253,29,29,1), rgba(252,176,69,1))',
+    };
+
+    const introSectionStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '20px 40px',
+        flex: 1,
+    };
+
+    const nameTextStyle = {
+        fontSize: '2.5rem',
+        fontFamily: 'Raleway',
+        fontWeight: '900',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        marginLeft: '150px',
+        zIndex: 3,
+        background: 'white',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+        MozBackgroundClip: 'text',
+        MozTextFillColor: 'transparent',
+        backgroundClip: 'text',
+        color: 'transparent',
+    };
+
+    const descripTextStyle = {
+        fontSize: '1.5rem',
+        fontFamily: 'Raleway',
+        color: 'white',
+        textAlign: 'left',
+        marginLeft: '150px',
+        maxWidth: '600px',
+    };
+
+    const textContainer = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        zIndex: 4,
+    };
+
+    const introImageStyle = {
+        width: '400px',
+        height: '400px',
+        borderRadius: '200px',
+        backgroundImage: 'url(/clear4.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center 70%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 4,
+        marginRight: '200px',
+        marginBottom: '0px',
+    };
+
+    const footerOuterStyle = {
+        background: 'linear-gradient(90deg, rgba(131,58,180,1), rgba(253,29,29,1), rgba(252,176,69,1))',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '30px',
+    };
+
+    const footerInnerInitialStyle = {
+        background: 'linear-gradient(135deg, rgba(131,58,180,1), rgba(253,29,29,1)',
+        height: '99%', // Slightly smaller
+        width: '84%', // Slightly narrower
+        borderRadius: '85px',
+        position: 'relative',
+        zIndex: 3,
+        boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.1)',
+        transition: 'all 1s ease', // Transition for the animation
+    };
+
+    const footerInnerFinalStyle = {
+        ...footerInnerInitialStyle,
+        height: '100%',
+        width: '85%',
+        boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.4)',
+    };
+
+    const downArrowStyle = {
+        color: 'white',
+        fontSize: '2em',
+        position: 'absolute',
+        top: 'calc(80vh + 30px)',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        zIndex: 4,
+        opacity: downArrowOpacity, // Controlled opacity
+        transition: 'opacity 1000ms ease' // Smooth transition for the fade-in effect
+    };
+
     return (
         <div style={outerContainerStyle}>
             <div style={introSectionStyle}>
@@ -113,19 +140,19 @@ const IntroSection = () => {
                     <div style={descripTextStyle}>
                         <p>Welcome!</p>
                         <p>I am a student in the Master of Science Programme in Interaction Technology and Design at Umeå University.</p>
+                        <p>View what I'm about below!</p>
                     </div>
                 </div>
                 <div style={introImageStyle}>
                     {/* The image is now displayed as a background */}
                 </div>
             </div>
-
             <div style={footerOuterStyle}>
-                <div style={footerInnerStyle}>
+                <div style={animateFooter ? footerInnerFinalStyle : footerInnerInitialStyle}>
                     {/* Additional content for the footer can go here */}
                 </div>
             </div>
-            <FontAwesomeIcon icon={faChevronDown} style={downArrowStyle} /> {/* Down arrow icon */}
+            {showDownArrow && <FontAwesomeIcon icon={faChevronDown} style={downArrowStyle} />} {/* Down arrow icon */}
         </div>
     );
 };
--- src/components/skillsSection.js
+++ src/components/skillsSection.js
@@ -1,76 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilRuler, faCode, faDatabase, faChevronDown } from '@fortawesome/free-solid-svg-icons'; // Import the down arrow icon
-
-const skillsSectionStyle = {
-    background: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
-    padding: '20px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '0',
-    height: '100vh',
-};
-
-const iconContainerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '57%',
-    marginBottom: '30px',
-    paddingLeft: '11%',
-    paddingRight: '11%',
-};
-
-const iconBackgroundStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: '50%',
-    background: 'radial-gradient(circle, rgba(0,0,0,0.15) 0%, rgba(255,255,255,0) 70%)',
-    padding: '10px',
-};
-
-const iconStyle = {
-    color: 'white',
-    fontSize: '2em',
-};
-
-const iconStyleFrontEnd = {
-    ...iconStyle,
-    fontSize: '2.5em',
-};
-
-const cardContainerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '75%',
-};
-
-const cardStyle = {
-    backgroundColor: 'white',
-    borderRadius: '30px',
-    padding: '26px',
-    boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.4)',
-    width: 'calc(30% - 10px)',
-    textAlign: 'center',
-    boxSizing: 'border-box',
-    position: 'relative',
-    overflow: 'hidden',
-    border: '15px dashed transparent', // Adjusted dashed border
-    backgroundClip: 'padding-box',
-};
-
-const gradientTextStyle = {
-    fontWeight: '900',
-    background: 'linear-gradient(90deg, rgba(131,58,180,1), rgba(253,29,29,1), rgba(252,176,69,1))',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-    display: 'inline',  
-    fontSize: '2.3em',
-};
+import { faPencilRuler, faCode, faDatabase, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const SkillsSection = () => {
+    const [cardVisibility, setCardVisibility] = useState({ card1: false, card2: false, card3: false });
+    const card1Ref = useRef(null);
+    const card2Ref = useRef(null);
+    const card3Ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setCardVisibility(prev => ({ ...prev, [entry.target.id]: true }));
+                }
+            });
+        }, { threshold: 1 });
+
+        observer.observe(card1Ref.current);
+        observer.observe(card2Ref.current);
+        observer.observe(card3Ref.current);
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
+    const getCardStyle = (cardNumber) => ({
+        backgroundColor: 'white',
+        borderRadius: '60px',
+        padding: '26px',
+        boxShadow: cardVisibility[`card${cardNumber}`] ? '0px 0px 25px rgba(0, 0, 0, 0.4)' : 'none',
+        width: cardVisibility[`card${cardNumber}`] ? 'calc(30% - 10px)' : '29%',
+        textAlign: 'left',
+        boxSizing: 'border-box',
+        position: 'relative',
+        overflow: 'hidden',
+        border: '15px dashed transparent',
+        backgroundClip: 'padding-box',
+        transition: `all ${cardNumber}s ease`,
+    });
+
+    const skillsSectionStyle = {
+        background: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
+        padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        position: 'relative',
+    };
+
+    const iconContainerStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '57%',
+        marginBottom: '30px',
+        paddingLeft: '11%',
+        paddingRight: '11%',
+    };
+
+    const iconBackgroundStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '50%',
+        background: 'radial-gradient(circle, rgba(0,0,0,0.15) 0%, rgba(255,255,255,0) 70%)',
+        padding: '10px',
+    };
+
+    const iconStyle = {
+        color: 'white',
+        fontSize: '2em',
+    };
+
+    const iconStyleFrontEnd = {
+        ...iconStyle,
+        fontSize: '2.5em',
+    };
+
+    const cardContainerStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '75%',
+    };
+
+    const gradientTextStyle = {
+        fontWeight: '900',
+        background: 'linear-gradient(90deg, rgba(131,58,180,1), rgba(253,29,29,1), rgba(252,176,69,1))',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+        textAlign: 'center', // Align gradient text center
+        display: 'block', // Changed to 'block' for center alignment
+        fontSize: '2.3em',
+        marginBottom: '10px', // Space between title and paragraph
+    };
+
+    const downArrowContainerStyle = {
+        position: 'absolute',
+        bottom: '30px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        fontSize: '2em',
+        color: 'white',
+    };
+
     return (
         <div style={skillsSectionStyle}>
             <div style={iconContainerStyle}>
@@ -85,18 +120,21 @@ const SkillsSection = () => {
                 </div>
             </div>
             <div style={cardContainerStyle}>
-                <div style={cardStyle}>
+                <div id="card1" ref={card1Ref} style={getCardStyle(1)}>
                     <span style={gradientTextStyle}>UI/UX</span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
-                <div style={cardStyle}>
+                <div id="card2" ref={card2Ref} style={getCardStyle(2)}>
                     <span style={{...gradientTextStyle, color: 'rgba(253,29,29,1)'}}>Front End</span>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
-                <div style={cardStyle}>
+                <div id="card3" ref={card3Ref} style={getCardStyle(3)}>
                     <span style={{...gradientTextStyle, color: 'rgba(252,176,69,1)'}}>Back End</span>
                     <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 </div>
+            </div>
+            <div style={downArrowContainerStyle}>
+                <FontAwesomeIcon icon={faChevronDown} />
             </div>
         </div>
     );
